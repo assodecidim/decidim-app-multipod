05b2de154d7cbf6d3da91c376bd2e3b3f10a6bf5
Andrés Pereira de Lucena <andreslucena@gmail.com>
2026-07-22 11:26:02 +0200

    Update capistrano configurations with latest DNS changes

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-server "staging.multipod.decidim.org", user: "app-decidim_multipod", roles: %w(app db web)
+server "staging.multipod.net", user: "app-decidim_multipod", roles: %w(app db web)
